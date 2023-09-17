f68a2804fb6d782ba52e354c04698748c2cf0654
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2023-09-17 23:03:27 +0600

    ADD: Add try/except blocks in Video class initialization

--- src/video.py
+++ src/video.py
@@ -8,12 +8,22 @@ class Video:
         Дальше все данные будут подтягиваться по API.
         """
         self.video_id = video_id
-        self.video_info = self.get_service().videos().list(id=self.video_id, part='snippet,statistics').execute()
-        self.title = self.video_info['items'][0]['snippet']['title']
-        self.viewCount = self.video_info['items'][0]['statistics']['viewCount']
-        self.likeCount = self.video_info['items'][0]['statistics']['likeCount']
-        #self.video_dict = self.youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
-        self.url = "https://www.youtube.com/video/" + self.video_id
+        try:
+            self.video_info = self.get_service().videos().list(id=self.video_id, part='snippet,statistics').execute()
+            self.title = self.video_info['items'][0]['snippet']['title']
+            self.view_count = self.video_info['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_info['items'][0]['statistics']['likeCount']
+            self.url = "https://www.youtube.com/video/" + self.video_id
+        except IndexError:
+            self.video_info = None
+            self.title = None
+            self.video_url = None
+            self.view_count = None
+            self.like_count = None
+
+    def __str__(self):
+        """Отображение информации об объекте класса для пользователей"""
+        return f'{self.title}'
 
     @classmethod
     def get_service(cls):
